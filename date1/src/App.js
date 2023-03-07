23921c5a1566d979ae9442b38f15590a500be76a
LeeSeokBong <kingstar6035@gmail.com>
2023-03-07 19:42:39 +0900

    moment, dayjs 라이브러리

---
+++ date1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MomentExample from './componets/Moment/MomentExample';
+import DayjsExample from './componets/Dayjs/DayjsExample';
+function App() {
+  return (
+    <div className="App">
+      <DayjsExample></DayjsExample>
+      <MomentExample></MomentExample>
+    </div>
+  );
+}
+
+export default App;
